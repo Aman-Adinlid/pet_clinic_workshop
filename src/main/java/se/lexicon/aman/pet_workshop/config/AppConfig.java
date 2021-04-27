54513d1665216270daed23c9b7a9eeeece3cda6f
aman adinlid <95pinkpanda@gmail.com>
2021-04-27 17:39:28 +0200

    ##Implmented config##

---
+++ src/main/java/se/lexicon/aman/pet_workshop/config/AppConfig.java
@@ -0,0 +1,13 @@
+package se.lexicon.aman.pet_workshop.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
