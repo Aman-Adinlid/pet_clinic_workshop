065c0a4d9afed0c33c2523740e3ae74e5afe25d4
aman adinlid <95pinkpanda@gmail.com>
2021-04-27 16:43:18 +0200

    ##Changed scope of H2 Database to test in the pom.xml and added model mapper dependencies to mvn##

---
+++ src/main/java/se/lexicon/aman/pet_workshop/PetWorkshopApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.aman.pet_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetWorkshopApplication.class, args);
+    }
+
+}
